12d8df01b6fffa7e9ef707f4a965758baf98d58c
ARELL31 <ferarellano654@gmail.com>
2026-04-24 16:10:21 -0600

    Se agrego la opción de poder realizar movimientos sobre ECS

--- gui/panels/__init__.py
+++ gui/panels/__init__.py
@@ -1,7 +1,15 @@
 from gui.panels.config import ConfigPanel, ConfigDialog
 from gui.panels.create import CreatePanel
 from gui.panels.delete import DeletePanel
+from gui.panels.ecs_manage import EcsManagePanel
 from gui.panels.export import ExportPanel
 from gui.panels.list_panel import ListPanel
 
-__all__ = ["ConfigPanel", "ConfigDialog", "CreatePanel", "DeletePanel", "ExportPanel", "ListPanel"]
+__all__ = [
+    "ConfigPanel", "ConfigDialog",
+    "CreatePanel",
+    "DeletePanel",
+    "EcsManagePanel",
+    "ExportPanel",
+    "ListPanel",
+]
